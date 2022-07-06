550b63dfa9f55953f214e899cc57d31f4296d408
Samuel Fauteux <58941456+enderfree@users.noreply.github.com>
2022-07-06 17:25:05 -0400

    Added Basic User Services
    More are to come.

--- src/main/java/com/fdmgroup/vvs/service/UserService.java
+++ src/main/java/com/fdmgroup/vvs/service/UserService.java
@@ -1,8 +1,11 @@
 package com.fdmgroup.vvs.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fdmgroup.vvs.model.User;
 import com.fdmgroup.vvs.repository.UserRepository;
 
 @Service
@@ -15,5 +18,23 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 	
+	public User addUser(User user) {
+		return userRepository.save(user);
+	}
 	
+	public void deleteUser(User user) {
+		userRepository.delete(user);
+	}
+	
+	public User updateUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	public User findUserById(int id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		if(user.isPresent()) return user.get();
+		
+		throw new NullPointerException("User not found.");
+	}
 }
