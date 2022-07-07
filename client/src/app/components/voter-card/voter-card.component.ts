04ba1c17009b59e674bd7a9bb853d5e7ed571d12
Michael Cartagena <mcartagenaez8@gmail.com>
2022-07-07 14:06:48 -0400

    style changes to login/home added voter-card component

---
+++ client/src/app/components/voter-card/voter-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-voter-card',
+  templateUrl: './voter-card.component.html',
+  styleUrls: ['./voter-card.component.css']
+})
+export class VoterCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
