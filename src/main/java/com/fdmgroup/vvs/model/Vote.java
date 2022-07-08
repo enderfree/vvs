a0f9205f8fef8830e782ac02a5c5c9ad29479d90
umchooh <71604148+umchooh@users.noreply.github.com>
2022-07-08 14:59:59 -0400

    Update the Vote.java

--- src/main/java/com/fdmgroup/vvs/model/Vote.java
+++ src/main/java/com/fdmgroup/vvs/model/Vote.java
@@ -3,6 +3,7 @@ package com.fdmgroup.vvs.model;
 import java.util.Date;
 import java.util.List;
 
+import javax.management.loading.PrivateClassLoader;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,111 +25,82 @@ public class Vote {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "VOTE_SEQ")
 	@Column(name = "VOTE_ID")
 	private int voteId;
-//	@OneToOne
-//	@JoinColumn(name = "FK_CANDIDATE_ID")
-//	private Candidate candidate;
+
+	// UserId of the candidate
+	// Will use to count the num of votes in the user model
+	@ManyToOne
+	@JoinColumn(name = "CANDIDATE_ID", referencedColumnName = "USER_ID", updatable = false, insertable = false)
+	private User user;
+
 	private Date date;
+
+	// UserId of the Voter
 	@OneToOne
 	@JoinColumn(name = "FK_USER_ID")
 	private User voter;
+
 	@ManyToOne
 	@JoinColumn(name = "FK_POLL_ID")
 	private Poll poll;
-	
-	@ManyToMany
-	@JoinTable(name = "CANDIDATES_USERS",
-	joinColumns = 
-	@JoinColumn(name="voteid"),
-	inverseJoinColumns = 
-	@JoinColumn(name = "userid")
-			)
-	private List<User> candidates;
-	
-	
-	
+
 	public Vote() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Vote(int voteId, Date date, User voter, Poll poll) {
+
+	public Vote(int voteId, User user, Date date, User voter, Poll poll) {
 		super();
 		this.voteId = voteId;
-//		this.candidate = candidate;
+		this.user = user;
 		this.date = date;
 		this.voter = voter;
 		this.poll = poll;
 	}
-	public Vote(int voteId, User voter, Poll poll) {
-		super();
-		this.voteId = voteId;
-//		this.candidate = candidate;
-		this.voter = voter;
-		this.poll = poll;
-		this.date = new Date();
-	}
-	/**
-	 * @return the poll
-	 */
-	public Poll getPoll() {
-		return poll;
-	}
-	/**
-	 * @param poll the poll to set
-	 */
-	public void setPoll(Poll poll) {
-		this.poll = poll;
-	}
-	@Override
-	public String toString() {
-		return "Vote [voteId=" + voteId +  ", date=" + date + ", voter=" + voter + "]";
-	}
-	/**
-	 * @return the voteId
-	 */
+
 	public int getVoteId() {
 		return voteId;
 	}
-	/**
-	 * @param voteId the voteId to set
-	 */
+
 	public void setVoteId(int voteId) {
 		this.voteId = voteId;
 	}
-	/**
-	 * @return the candidate
-	 */
-//	public Candidate getCandidate() {
-//		return candidate;
-//	}
-//	/**
-//	 * @param candidate the candidate to set
-//	 */
-//	public void setCandidate(Candidate candidate) {
-//		this.candidate = candidate;
-//	}
-	/**
-	 * @return the date
-	 */
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	public Date getDate() {
 		return date;
 	}
-	/**
-	 * @param date the date to set
-	 */
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	/**
-	 * @return the voter
-	 */
+
 	public User getVoter() {
 		return voter;
 	}
-	/**
-	 * @param voter the voter to set
-	 */
+
 	public void setVoter(User voter) {
 		this.voter = voter;
 	}
-	
+
+	public Poll getPoll() {
+		return poll;
+	}
+
+	public void setPoll(Poll poll) {
+		this.poll = poll;
+	}
+
+	@Override
+	public String toString() {
+		return "Vote [voteId=" + voteId + ", user=" + user + ", date=" + date + ", voter=" + voter + ", poll=" + poll
+				+ "]";
+	}
+
 }
